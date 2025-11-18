c97be9d849833f1fb4d2c31ef9c9e66c680b2bab
MIMminE <weluv.ww@gmail.com>
2025-11-18 14:09:38 +0900

    fetch: sso-back-app-2 프로젝트 추가

---
+++ sso-back-app-2/src/test/java/nuts/project/sso/sso_back_app_2/SsoBackApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package nuts.project.sso.sso_back_app_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoBackApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
